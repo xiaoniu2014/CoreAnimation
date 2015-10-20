25379bf3d0eb2533c6ec9ec1997a3137212f3c8f
xiaoniu2014 <116445168@qq.com>
2015-10-20 14:45:54 +0800

    Initial Commit

---
+++ CoreAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimation
+//
+//  Created by hw on 15/10/20.
+//  Copyright © 2015年 hongw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreAnimation
+//
+//  Created by hw on 15/10/20.
+//  Copyright © 2015年 hongw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
